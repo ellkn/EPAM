3e95474f9612dd0d8c4979e732816321f5a32129
Нелли Камалян <nelli.kamalyan@mail.ru>
2022-01-30 04:36:24 +0400

    Task 3.1.1

---
+++ Task 3/Task 3.1/Task 3.1.1 WEAKEST LINK/Task 3.1.1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Task_3._1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Выберите действие:");
+                Console.WriteLine("\t1. Игра");
+                Console.WriteLine("\t2. Выход");
+                input = GetConsoleIntValue();
+                Console.ForegroundColor= ConsoleColor.White;
+                switch (input)
+                {
+                    case 1:
+                        Console.Write("Введите количество игроков (N): ");
+                        int capacity = GetConsoleIntValue();
+                        Console.Write("Введите номер игрока, выбывающего в каждом раунде: ");
+                        int id = GetConsoleIntValue();
+                        WeakestLink weakestLink = new WeakestLink(capacity, id);
+                        weakestLink.Run();
+                        break;
+                    case 2:
+                        break;
+                    default:
+                        Console.WriteLine("Try Again!");
+                        break;
+                }
+            } while (input != 2);
+        }
+        public static int GetConsoleIntValue()
+        {
+            Boolean isNegative = true;
+            while (isNegative)
+            {
+                string value = Console.ReadLine();
+                int result = 0;
+                if (!int.TryParse(value, out result) || result <= 0)
+                {
+                    Console.WriteLine("Ошибка ввода! Введите корректное значение");
+                }
+                else
+                {
+                    isNegative = false;
+                    return result;
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ Task 3/Task 3.1/Task 3.1.1 WEAKEST LINK/Task 3.1.1/WeakestLink.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3._1._1
+{
+    public class WeakestLink
+    {
+        private List<People> _people;
+        private int _id;
+        public WeakestLink(int capacity, int id)
+        {
+            _people = new List<People>(capacity);
+            for (int i = 0; i < capacity; i++)
+            {
+                _people.Add(new People(i + 1));
+            }
+            _id = id;
+        }
+        public void Run()
+        {
+            int removedId = _id - 1;
+            while(_people.Count > removedId)
+            {
+                for (int i = 1; _id <= _people.Count; i++) // where i - numbeer of round
+                {
+                    _people.Remove(_people[removedId]);
+                    Console.WriteLine($"Раунд {i}. Вычеркнут человек с номером {_id}. Осталось: {_people.Count} игроков");
+                }
+            }
+            CheckingCorrectness();
+        }
+        public void CheckingCorrectness()
+        {
+            if(_people.Count < _id)
+            {
+                Console.WriteLine("Игра окончена. Невозможно удалить пользователя по индексу, если индекс больше количества пользователей!");
+            }
+        }
+    }
+}
